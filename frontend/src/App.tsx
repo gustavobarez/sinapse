97626ef2d70d6eb6be31a15b77f313cc0cca1185
gustavobarez <gbarez44@gmail.com>
2025-07-25 15:43:18 -0300

    fix: change theme color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Sidebar } from "@components/Sidebar";
 import { Navbar } from "@components/Navbar";
+import { Sidebar } from "@components/Sidebar";
 import { ThemeProvider } from "@components/theme-provider";
+import { useState } from "react";
 
 function App() {
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
@@ -11,7 +11,11 @@ function App() {
   };
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      storageKey="vite-ui-theme"
+    >
       <div className="flex h-screen bg-white dark:bg-gray-800">
         <Sidebar isPinnedOpen={isSidebarExpanded} onMenuClick={toggleSidebar} />
         <main className="flex-1 p-6">
